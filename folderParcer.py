f1bc4d553b680bfc98ed23c6e0d39975e0bfe7a3
Nikita <draco1997pst@gmail.com>
2023-10-31 23:57:23 +0200

    нормализировал запросы в БД создал списки пользевателей и типи сюжетов для бд чтобы обращаться по id

--- folderParcer.py
+++ folderParcer.py
@@ -1,11 +1,18 @@
 import os
 import sqlite3
-# path = '/Users/mikita/Work/Продакшн/END/'
-Nikita = '/volume2/NAS_1/Готовые_сюжети/Никита/'
-Igor = '/volume2/NAS_1/Готовые_сюжети/Игорь/'
-Jenya = '/volume2/NAS_1/Готовые_сюжети/Євген/'
-Kostya = '/volume2/NAS_1/Готовые_сюжети/Костя/'
-Taylor = '/volume2/NAS_1/Готовые_сюжети/Taylor'
+import users
+
+path = '/Users/mikita/Work/Продакшн/END/'
+# Nikita = '/volume2/NAS_1/Готовые_сюжети/Никита/'
+# Igor = '/volume2/NAS_1/Готовые_сюжети/Игорь/'
+# Jenya = '/volume2/NAS_1/Готовые_сюжети/Євген/'
+# Kostya = '/volume2/NAS_1/Готовые_сюжети/Костя/'
+# Taylor = '/volume2/NAS_1/Готовые_сюжети/Taylor'
+
+# db = sqlite3.connect("/volume1/scripts/Youtube_raports/Youtube.db")
+db = sqlite3.connect("/Users/mikita/Main/PythonProjects/raportsMeneger/Youtube.db")
+c = db.cursor()
+
 l =[]
 def WalkFiles(path):
     # l = []
@@ -24,9 +31,7 @@ def getListFiles(path):
         if t == 5:
             l.append(s)
     return l
-db = sqlite3.connect("/volume1/scripts/Youtube_raports/Youtube.db")
-# db = sqlite3.connect("/Users/mikita/Main/PythonProjects/folderParcer/Youtube.db")
-c = db.cursor()
+
 def createTable():
     c.execute('''CREATE TABLE IF NOT EXISTS VideoStorage
     (
@@ -49,39 +54,54 @@ def writheBdList(path):
         nameDir = i[3]
         nameJurn = i[2]
         
-        s = i[1]
+        typVideo = i[1]
+
         key = i[4].replace(" ","")
         key = key.replace("@SynoEAStream","")
         key = key.replace("@SynoResource","")
 
-        if nameDir == "Скрипниченко" or nameDir == "cкрипниченко" or nameDir == "cкрипніченко":
-            nameDir = "Скрипніченко"
+        if users.getId(nameJurn) != None:
+            nameJurn = users.getId(nameJurn)
+        if users.getId(nameDir) != None:
+            nameDir = users.getId(nameDir)
+        if users.getIdType(typVideo)!= None:
+            typVideo = users.getIdType(typVideo)
+
+
+        # print(nameJurn)
+        # print(nameDir)
+        # print(i)
+
+
+        #
+        # if nameDir == "Скрипниченко" or nameDir == "cкрипниченко" or nameDir == "cкрипніченко":
+        #     nameDir = "Скрипніченко"
+        #
+        # if nameJurn == "Табацкая" or nameJurn == "Сухонис" or nameJurn == "сухоніс" or nameJurn == "сухонис" or nameJurn == "Табацька"or nameJurn == "Дарина":
+        #     nameJurn = "Сухоніс"
+        #
+        # if nameDir == "Ігор":
+        #     nameDir = "Бездільний"
+        #
+        # if nameDir == "Костя":
+        #     nameDir = "Фурдуй"
             
-        if nameJurn == "Табацкая" or nameJurn == "Сухонис" or nameJurn == "сухоніс" or nameJurn == "сухонис" or nameJurn == "Табацька"or nameJurn == "Дарина":
-            nameJurn = "Сухоніс"
-            
-        if nameDir == "Ігор":
-            nameDir = "Бездільний"
-            
-        if nameDir == "Костя":
-            nameDir = "Фурдуй"
-            
-        if s == "НМ" or s == "ДО" or s == "ДП":
-        	s = "СЮЖ"
+        if typVideo == "НМ" or typVideo == "ДО" or typVideo == "ДП":
+            typVideo = "СЮЖ"
 
 
         c.execute(f"SELECT key FROM VideoStorage WHERE key = '{key}'")
         if c.fetchone() is None:
-            c.execute(f"INSERT INTO VideoStorage VALUES (?, ?, ?, ?, ?)", (i[0], s, nameJurn, nameDir, key))
+            c.execute(f"INSERT INTO VideoStorage VALUES (?, ?, ?, ?, ?)", (i[0], typVideo, nameJurn, nameDir, key))
         db.commit()
 
-# writheBdList(path)
-writheBdList(Nikita)
-writheBdList(Igor)
-writheBdList(Jenya)
-writheBdList(Kostya)
-writheBdList(Taylor)
+writheBdList(path)
+# writheBdList(Nikita)
+# writheBdList(Igor)
+# writheBdList(Jenya)
+# writheBdList(Kostya)
+# writheBdList(Taylor)
 
 db.close()
-
-print("folders Updates!!!")
+if __name__ == '__main__':
+    print("folders Updates!!!")

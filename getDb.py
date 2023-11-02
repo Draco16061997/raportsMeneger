fec39244aa86daffa70bd0f672ae52061d6cc446
Nikita <draco1997pst@gmail.com>
2023-11-02 22:51:18 +0200

    добавил фильрацию по ресурсам

--- getDb.py
+++ getDb.py
@@ -51,14 +51,37 @@ def ExecuteDirDB(isShorts, IDDir, ot, do):
                         ''')
 
     rows = c.fetchall()
-    # t = 0
-    # for i in rows:
-    #     print(i)
-    #     t += 1
-    # print(t)
-
     db.close()
     return rows
+
+def ExecuteResDB(isShorts, res, ot, do):
+    db = sqlite3.connect(pathDb)
+    c = db.cursor()
+    list = ('Дніпро Оперативний - життя великого міста','Наше Місто','Дніпровська Панорама')
+
+    selecter = None
+    if isShorts:
+        selecter = '='
+    else:
+        selecter = '<>'
+
+    c.execute(f'''SELECT YoutubeVideo.data, YoutubeVideo.name, typeVideo.name,YoutubeVideo.chanel, Jurn.name AS JurnName, Dir.name AS DirName, YoutubeVideo.url
+            FROM YoutubeVideo
+            LEFT JOIN VideoStorage ON VideoStorage.key = YoutubeVideo.key
+            LEFT JOIN typeVideo ON typeVideo.id = VideoStorage.TypeClip
+            LEFT JOIN emploues AS Jurn ON Jurn.id = VideoStorage.NameJurn
+            LEFT JOIN emploues AS Dir ON Dir.id = VideoStorage.NameDirector
+            WHERE YoutubeVideo.chanel = '{list[res]}' AND typeVideo.id {selecter} 2  AND YoutubeVideo.data BETWEEN '{ot}' AND '{do}'
+            ORDER BY YoutubeVideo.data DESC;
+                        ''')
+
+    rows = c.fetchall()
+    db.close()
+    return rows
+
+#
+# for i in ExecuteResDB(True,0,'2023-10-24','2023-11-01'):
+#     print(i)
 
 # for i in ExecuteDirDB(False,12,'2023-10-24','2023-11-01'):
 #     print(i)

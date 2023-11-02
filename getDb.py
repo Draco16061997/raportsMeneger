ad339bd4f5a319a8ac23813c7ffbdc4cbc3251c4
Nikita <draco1997pst@gmail.com>
2023-11-02 22:38:19 +0200

    создал новый шаблон запросов в базу даных по журанлистам і режмонтам

---
+++ getDb.py
@@ -0,0 +1,69 @@
+import sqlite3
+
+pathDb = "/Users/mikita/Main/PythonProjects/raportsMeneger/Youtube.db"
+
+def ExecuteJurnDB(isShorts, IDJurn, ot, do):
+    db = sqlite3.connect(pathDb)
+    c = db.cursor()
+    selecter = None
+    if isShorts:
+        selecter = '='
+    else:
+        selecter = '<>'
+
+    c.execute(f'''SELECT YoutubeVideo.data, YoutubeVideo.name, typeVideo.name,YoutubeVideo.chanel, Jurn.name AS JurnName, Dir.name AS DirName, YoutubeVideo.url
+            FROM YoutubeVideo
+            LEFT JOIN VideoStorage ON VideoStorage.key = YoutubeVideo.key
+            LEFT JOIN typeVideo ON typeVideo.id = VideoStorage.TypeClip
+            LEFT JOIN emploues AS Jurn ON Jurn.id = VideoStorage.NameJurn
+            LEFT JOIN emploues AS Dir ON Dir.id = VideoStorage.NameDirector
+            WHERE VideoStorage.NameJurn = {IDJurn} AND typeVideo.id {selecter} 2  AND YoutubeVideo.data BETWEEN '{ot}' AND '{do}'
+            ORDER BY YoutubeVideo.data DESC;
+                        ''')
+
+    rows = c.fetchall()
+    # t = 0
+    # for i in rows:
+    #     print(i)
+    #     t += 1
+    # print(t)
+
+    db.close()
+    return rows
+
+def ExecuteDirDB(isShorts, IDDir, ot, do):
+    db = sqlite3.connect(pathDb)
+    c = db.cursor()
+    selecter = None
+    if isShorts:
+        selecter = '='
+    else:
+        selecter = '<>'
+
+    c.execute(f'''SELECT YoutubeVideo.data, YoutubeVideo.name, typeVideo.name,YoutubeVideo.chanel, Jurn.name AS JurnName, Dir.name AS DirName, YoutubeVideo.url
+            FROM YoutubeVideo
+            LEFT JOIN VideoStorage ON VideoStorage.key = YoutubeVideo.key
+            LEFT JOIN typeVideo ON typeVideo.id = VideoStorage.TypeClip
+            LEFT JOIN emploues AS Jurn ON Jurn.id = VideoStorage.NameJurn
+            LEFT JOIN emploues AS Dir ON Dir.id = VideoStorage.NameDirector
+            WHERE VideoStorage.NameDirector = {IDDir} AND typeVideo.id {selecter} 2  AND YoutubeVideo.data BETWEEN '{ot}' AND '{do}'
+            ORDER BY YoutubeVideo.data DESC;
+                        ''')
+
+    rows = c.fetchall()
+    # t = 0
+    # for i in rows:
+    #     print(i)
+    #     t += 1
+    # print(t)
+
+    db.close()
+    return rows
+
+# for i in ExecuteDirDB(False,12,'2023-10-24','2023-11-01'):
+#     print(i)
+# for i in ExecuteJurnDB(True, 1,'2023-10-24','2023-11-01'):
+#     print(i)
+
+if __name__ == '__main__':
+    None
